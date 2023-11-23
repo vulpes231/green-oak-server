3b040f9b28bc79d02bc95eb107002d254e483816
Vulpes231 <whykayshot@gmail.com>
2023-11-23 21:39:45 +0100

    added fe to whitelist

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3600"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3600",
+  "https://greenoaktrust.us",
+];
 
 module.exports = allowedOrigins;
