3fbae94e0e76ee2daef514a03addd9284107e7de
Vulpes231 <whykayshot@gmail.com>
2023-11-23 20:35:42 +0100

    fix

--- controllers/deposit-controller.js
+++ controllers/deposit-controller.js
@@ -3,31 +3,36 @@ const Account = require("../models/Account");
 const Deposit = require("../models/Deposit");
 
 const depositCheck = async (req, res) => {
-  const { deposit_to, amount, date } = req.body;
+  const { deposit_to, amount } = req.body;
   const { id } = req.params;
 
-  if (!deposit_to || !amount || !date || !id)
+  console.log(req.body);
+  console.log(id);
+
+  if (!deposit_to || !amount)
     return res.status(400).json({ message: "Enter deposit details!" });
 
-  const userAccount = await Account.findOne({ _id: id }).exec();
+  const userAccount = await Account.findOne({ account_owner: id }).exec();
 
   if (!userAccount)
     return res.status(404).json({ message: "Invalid user account!" });
 
-  const getAvailableDate = addDays(date, 3);
+  const getAvailableDate = addDays(new Date(), 3);
   const newDate = format(getAvailableDate, "yyyy:MM:dd");
+  const depositDate = format(new Date(), "yyyy:MM:dd");
 
   try {
     const newDeposit = {
       depositor: id,
       deposit_account: deposit_to,
       amount: amount,
-      deposit_date: date,
+      deposit_date: depositDate,
       available_date: newDate,
     };
 
     const deposit = await Deposit.create(newDeposit);
     console.log(deposit);
+    res.status(201).json({ message: "Check deposit successful" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "An error occured" });
