34ec909e33d5f439ae66edf55e17e345073b470f
Vulpes231 <whykayshot@gmail.com>
2024-01-11 07:47:23 +0100

    fix-cors

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3600",
   "https://greenoaktrust.us",
+  "https://admin.greenoaktrust.us",
 ];
 
 module.exports = allowedOrigins;
